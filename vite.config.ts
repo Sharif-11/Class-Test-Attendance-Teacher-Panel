366a43eec6003e5bc8a0aafeaeff903ed57a86ac
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-12-03 20:21:17 +0600

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // This is the directory Render expects
+    emptyOutDir: true,
+  },
   server: {
-    host: "0.0.0.0",
-    port: 5174,
+    host: "0.0.0.0", // Required for Render
+    port: parseInt(process.env.PORT) || 5174, // Render will provide the PORT via an environment variable
   },
 });
